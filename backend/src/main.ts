56d87f99d5aa53e261e25469a0e06416c2cdb36b
Sskame1 <tolikstrgonov111@gmail.com>
2025-02-28 10:13:45 +0300

    Добавил в Nest Swagger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle("Backend Патриот")
+    .setDescription("Swagger для Backend части проекта Патриот")
+    .setVersion("0.1")
+    .addTag("Альфа прототип")
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api", app, document);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
